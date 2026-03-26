f9be003fdd3720935ef1d8220206951db32dd19d
MSI\nazri <nazrinamirrullah@gmail.com>
2026-03-26 18:18:32 +0800

    fix: Modal

--- Scripts/UI Scripts/modal.gd
+++ Scripts/UI Scripts/modal.gd
@@ -1,18 +1,24 @@
 extends Control
 
 @onready var label = $ColorRect/CenterContainer/Panel/VBoxContainer/Label
-@onready var close_button = $ColorRect/CenterContainer/Panel/VBoxContainer/Button
+@onready var button = $ColorRect/CenterContainer/Panel/VBoxContainer/Button
+@onready var animation_player = $AnimationPlayer
+@onready var color_rect = $ColorRect
+@onready var panel = $ColorRect/CenterContainer/Panel
 
 signal confirmed
 
 func _ready():
 	visible = false
-	close_button.pressed.connect(_on_close_button_pressed)
+	button.pressed.connect(_on_close_button_pressed)
 
 func show_modal(message: String):
 	label.text = message
 	visible = true
+	animation_player.play("open")
 
 func _on_close_button_pressed():
+	animation_player.play("close")
+	await animation_player.animation_finished
 	visible = false
 	confirmed.emit()
